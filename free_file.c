e10a845ea5cda6ce63d8890d61b6ed1aaf3ea6bf
sackisman <ostechcomm@gmail.com>
2022-12-14 01:33:12 -0800

    function that free all memory allocated

---
+++ free_file.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+
+
+/**
+ * free_file - function that free all memory allocated
+ * @fd: file open
+ * @line_buf: the line read
+ * @stack: pointer to the head of stack_t
+ */
+
+
+
+void free_file(FILE *fd, char *line_buf, stack_t **stack)
+
+{
+	(void) line_buf;
+
+	if (stack)
+		free_dlistint(stack);
+	fclose(fd);
+}
