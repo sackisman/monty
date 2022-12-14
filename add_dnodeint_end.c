b178e913ba492baacf5a080d54639c0449c0d097
sackisman <ostechcomm@gmail.com>
2022-12-14 00:18:19 -0800

    function that add new node at the end of the stacklist

---
+++ add_dnodeint_end.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+
+
+
+/**
+ * add_dnodeint_end - function that adds a new node
+ * at the end of a stack_t list
+ * @stack: pointer to the stack_t list
+ * @n: value to add
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+
+
+stack_t *add_dnodeint_end(stack_t **stack, const int n)
+
+{
+	stack_t *last = *stack;
+	stack_t *new = malloc(sizeof(stack_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (*stack == NULL)
+	{
+		new->prev = NULL;
+		*stack = new;
+	}
+	else
+	{
+		while (last->next != NULL)
+			last = last->next;
+
+		last->next = new;
+		new->prev = last;
+
+	}
+	while ((*stack)->prev)
+		*stack = (*stack)->prev;
+
+	return (*stack);
+
+}
