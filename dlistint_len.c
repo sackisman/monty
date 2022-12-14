e0da2284e2b1da7abcb231b368cd1d6804ee20c5
sackisman <ostechcomm@gmail.com>
2022-12-14 01:13:21 -0800

    return list

---
+++ dlistint_len.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+
+
+/**
+ * dlistint_len - list
+ * @h: list
+ * Return: list
+ */
+
+
+
+int dlistint_len(stack_t *h)
+
+{
+	int n;
+
+	for (n = 0; h; n++)
+	{
+		h = h->next;
+	}
+	return (n);
+}
