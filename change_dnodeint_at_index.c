4be18ce4b46289b8dd933228304ae26932f8eec4
sackisman <ostechcomm@gmail.com>
2022-12-14 00:28:39 -0800

    function that change
     * the value of a node at index "index" of a stack_t linked list

---
+++ change_dnodeint_at_index.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+
+
+/**
+ * change_dnodeint_at_index - function that change
+ * the value of a node at index "index" of a stack_t linked list
+ * @stack: pointer to the stack_t list
+ * @index: position to indexete
+ * @value: new value to put
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+
+
+
+int change_dnodeint_at_index(stack_t **stack, unsigned int index, int value)
+{
+	if (*stack == NULL)
+		return (-1);
+
+	while (index > 0)
+	{
+		*stack = (*stack)->next;
+		index--;
+		if (*stack == NULL)
+			return (-1);
+
+	}
+
+	(*stack)->n = value;
+
+	return (1);
+}
