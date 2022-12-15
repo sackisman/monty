38caf5a5dfdb2b0e6676a0ee97da23b85740392a
sackisman <ostechcomm@gmail.com>
2022-12-15 00:58:16 -0800

    prints the value at the top of the stack

---
+++ op_pint.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+
+
+/**
+ * op_pint - prints the value at the top of the stack
+ * @stack: pointer to the address of the stack
+ * Return: 0 on success, -2 on error
+ */
+
+
+
+int op_pint(stack_t **stack)
+
+{
+	if (*stack == NULL)
+		return (-2);
+	printf("%d\n", (*stack)->n);
+	return (0);
+
+}
