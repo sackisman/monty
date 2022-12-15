57237bd1d271ae82e48e946261a6fbfd9f0be94a
sackisman <ostechcomm@gmail.com>
2022-12-15 02:43:04 -0800

    error for unknow instruction

---
+++ unknow_instruction.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+
+
+/**
+ * unknow_instruction - error for unknow instruction
+ * @stack: pointer to the address of the stack
+ * Return: -99 for unknow instruction error code
+ */
+
+
+
+int unknow_instruction(stack_t **stack)
+
+{
+	(void) **stack;
+	return (-99);
+
+}
