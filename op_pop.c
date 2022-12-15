e847dbf313f2e060487412302629d50310be79dd
sackisman <ostechcomm@gmail.com>
2022-12-15 01:05:26 -0800

    removes the top element of the stack

---
+++ op_pop.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+
+
+/**
+ * op_pop - removes the top element of the stack
+ * @stack: pointer to the address of the stack
+ * Return: 0 on success, -3 on error
+ */
+
+
+
+int op_pop(stack_t **stack)
+
+{
+	if (*stack == NULL)
+		return (-3);
+	delete_dnodeint_at_index(stack, 0);
+	return (0);
+
+}
