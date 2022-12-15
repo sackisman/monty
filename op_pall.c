e0c9927d306afb32a31c01d885e9533e2b0b76d8
sackisman <ostechcomm@gmail.com>
2022-12-15 00:45:43 -0800

    prints all the values on the stack starting from the top of the stack

---
+++ op_pall.c
@@ -0,0 +1,28 @@
+nclude "monty.h"
+
+
+
+/**
+ * op_pall - prints all the values on the stack
+ * starting from the top of the stack
+ * @stack: pointer to the address of the stack
+ * Return: 0
+ */
+
+
+
+int op_pall(stack_t **stack)
+
+{
+	stack_t *cpy = *stack;
+
+	if (cpy)
+	{
+		while (cpy)
+		{
+			printf("%d\n", cpy->n);
+			cpy = cpy->next;
+		}
+	}
+	return (0);
+}
