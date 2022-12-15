57b760816ee761baab4741b61aedc8a2751b66f1
sackisman <ostechcomm@gmail.com>
2022-12-15 00:38:31 -0800

    doesn’t do anything

---
+++ op_nop.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+
+
+/**
+ * op_nop - doesn’t do anything
+ * @stack: pointer to the address of the stack
+ * Return: 0
+ */
+
+
+
+int op_nop(stack_t **stack)
+
+{
+	(void) **stack;
+	return (0);
+}
