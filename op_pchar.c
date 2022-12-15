7a0be888a5023329fc964e52094bc2194978c840
sackisman <ostechcomm@gmail.com>
2022-12-15 00:54:07 -0800

    prints the char at the top of the stack, followed by a new line

---
+++ op_pchar.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+
+
+/**
+ * op_pchar - prints the char at the top of the stack, followed by a new line
+ * @stack: pointer to the address of the stack
+ * Return: 0 on success, -12 or -13 on error
+ */
+
+
+
+int op_pchar(stack_t **stack)
+
+{
+	stack_t *cpy = *stack;
+
+	if (*stack == NULL)
+		return (-12);
+	if (cpy->n >= 0 && cpy->n < 127)
+	{
+		putchar(cpy->n);
+		putchar('\n');
+	}
+	else
+		return (-13);
+	return (0);
+}
