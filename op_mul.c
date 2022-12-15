1dac00ede0ddb573c542adcd216db0e21a90b140
sackisman <ostechcomm@gmail.com>
2022-12-15 00:33:27 -0800

    multiplies the second top element of the stack

---
+++ op_mul.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+
+
+/**
+ * op_mul - multiplies the second top element of the stack
+ * with the top element of the stack
+ * @stack: pointer to the address of the stack
+ * Return: 0 on success, -9 on error
+ */
+
+
+
+int op_mul(stack_t **stack)
+
+{
+	int result;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		return (-9);
+	result = ((*stack)->n) * ((*stack)->next->n);
+	delete_dnodeint_at_index(stack, 0);
+	change_dnodeint_at_index(stack, 0, result);
+	return (0);
+
+}
