8362138e5fde633d70055737d4fc82feac2910e5
sackisman <ostechcomm@gmail.com>
2022-12-15 01:26:36 -0800

    sets the format of the data to a queue (FIFO)

---
+++ op_queue.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+
+
+/**
+ * op_queue - sets the format of the data to a queue (FIFO)
+ * @stack: pointer to the address of the stack
+ * Return: 0
+ */
+
+
+
+int op_queue(stack_t **stack)
+
+{
+	(void) **stack;
+	stack_queu = 1;
+	return (0);
+}
