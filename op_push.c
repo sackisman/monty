61b3b52c70fb4c439659f1aa3049a2f54a6a99eb
sackisman <ostechcomm@gmail.com>
2022-12-15 01:19:40 -0800

    pushes an element to the stack

---
+++ op_push.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+
+
+/**
+ * op_push - pushes an element to the stack
+ * @stack: pointer to the address of the stack
+ * @line_number: number of the line where we are in the file
+ * @number: number to add to the stack
+ * Return: pointer to new stack on success, exit on error
+ */
+
+
+
+stack_t *op_push(stack_t **stack, unsigned int line_number, int number)
+
+{
+	if (stack_queu == 0)
+		*stack = add_dnodeint(stack, number);
+	else
+		*stack = add_dnodeint_end(stack, number);
+	if (!*stack)
+	{
+		fprintf(stderr, "%d", line_number);
+		exit(EXIT_FAILURE);
+	}
+	return (*stack);
+}
