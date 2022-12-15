b3cbb94ae6bb40c88ed33d551e3212f881b86d20
sackisman <ostechcomm@gmail.com>
2022-12-15 01:46:38 -0800

    sets the format of the data to a stack (LIFO)

---
+++ op_stack.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+
+
+/**
+ * op_stack - sets the format of the data to a stack (LIFO)
+ * this is the default behavior of the program
+ * @stack: pointer to the address of the stack
+ * Return: 0
+ */
+
+
+
+int op_stack(stack_t **stack)
+
+{
+	(void) **stack;
+	stack_queu = 0;
+	return (0);
+
+}
