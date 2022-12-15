faab74021aa741cb2a6079b7dc88b0cbc5c33a9e
sackisman <ostechcomm@gmail.com>
2022-12-15 01:58:56 -0800

    swaps the top two elements of the stack

---
+++ op_swap.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+
+
+/**
+ * op_swap - swaps the top two elements of the stack
+ * @stack: pointer to the address of the stack
+ * Return: 0 on success, -4 on error
+ */
+
+
+
+int op_swap(stack_t **stack)
+
+{
+	int tmp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		return (-4);
+	tmp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
+	return (0);
+}
